122a43c1a8ff3a88936222f3eb3b1c8b35e38bc9
Артём Ежов <dragster1001@yandex.ru>
2023-01-13 00:24:34 +0300

    cash_bot 1.3 can count differntly depending on register date

--- cash_bot.py
+++ cash_bot.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from logging import StreamHandler
+import datetime
 import os
 import re
 import sys
@@ -9,6 +10,7 @@ from telegram.ext import CommandHandler, Updater, MessageHandler, Filters
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Bot
 
 from db import BotDB
+from models import User
 db = BotDB('telebot.db')
 
 logger = logging.getLogger(__name__)
@@ -26,59 +28,88 @@ def wake_up(update, context):
     name = update.message.chat.first_name
     chat = update.effective_chat
     if not db.user_exists(user_id):
-        db.add_user(user_id, username)
+        db.add_user(user_id, username)  # дата расчётов по умолчанию дата регистрации
+        reply_markup = ReplyKeyboardMarkup(keyboard=[['Поделить всё, что было'],['Считать мой долг с текущей даты']], resize_keyboard=True)
+        message = f'Привет, {name}! У ты тут не первый и возможно уже были платежи (сумму можно посмотреть в меню). Выбери что делать, пожалуйста!'
+        send_message(context, chat.id, message, reply_markup=reply_markup)
+        return
 
     text = f'Привет, {name}! Напиши циферками заплаченную сумму👋'
-    send_message(context, chat.id, text)
+    send_message(context, chat.id, text, reply_markup=ReplyKeyboardRemove())
 
 
-def calculation(context, update, amount=0):
+def calculation(update, context, amount=0):
     user_id = context._user_id_and_data[0]
-    name = update.message.chat.first_name
+    name = update.effective_chat.username
+    # получаем всех юзеров и регим их аттрибуты
+    user_info = dict()
     all_users = [user[0] for user in (db.get_users())]
-    
+    for user_id in all_users:
+        user_id, username, pays_since = db.get_user_data(user_id)
+        user_info[username] = [user_id, pays_since]
+        
     if amount != 0:
-        db.add_sum(user_id, amount)
+        user_id = context._user_id_and_data[0]
+        db.add_sum(user_id, amount)  # потом включить!!!!!!
         message = f'Ребятки, {name}😎 только что заплатил {amount}'
         for user_id in all_users:
-            send_message(context, chat_id=user_id, text=message)
-        for user_id in all_users:
-            total_paid = db.total() #
-            numer_of_users = len(all_users) #
-            one_person_owes = total_paid / numer_of_users #      
-            user_total = db.total(user_id)
-            user_balance = user_total - one_person_owes
-            if user_balance < 0:
-                message = f'С тебя {"%.2f" % abs(user_balance)}'
-                send_message(context, chat_id=user_id, text=message)
-            elif user_balance > 0:
-                message = f'👍Ты в плюсе на {"%.2f" % user_balance}'
-                send_message(context, chat_id=user_id, text=message)
-            elif user_balance == 0:
-                message = f'Уникальный момент, всё поровну!🥳'
-                send_message(context, chat_id=user_id, text=message)
-    else: 
-            total_paid = db.total() #
-            numer_of_users = len(all_users) #
-            one_person_owes = total_paid / numer_of_users #                                                            #тут можно упростить
-            user_total = db.total(user_id)
-            user_balance = user_total - one_person_owes
-            if user_balance < 0:
-                message = f'С тебя {"%.2f" % abs(user_balance)}'
-                send_message(context, chat_id=user_id, text=message)
-            elif user_balance > 0:
-                message = f'👍Ты в плюсе на {"%.2f" % user_balance}'
-                send_message(context, chat_id=user_id, text=message)
-            elif user_balance == 0:
-                message = f'Уникальный момент, всё поровну!🥳'
-                send_message(context, chat_id=user_id, text=message)        
+            send_message(context=context, chat_id=user_id, text=message)
+        since = user_info[list(user_info)[0]][1]
+        until = str(datetime.datetime.now())
+        times = list()
+        for values in user_info.values():
+            times.append(values[1])
 
+        period_qty = len(times) - 1
+        number_of_users = 1
+        for i in range(period_qty):
+    
+            since = user_info[list(user_info)[i+1]][1]
+            until = str(datetime.datetime.now())
+            if not i == (period_qty-1):
+                until = user_info[list(user_info)[i+2]][1]
+
+            number_of_users += 1  # перенести выше
+            totally_paid = db.get_period_payments(since=since, until=until)
+            for i in range(number_of_users):
+                username = list(user_info)[i]
+                user_id = user_info[username][0]
+                totally_user = db.get_period_payments(since=since, until=until, user_id=user_id)
+                one_person_owes = totally_paid / number_of_users
+                user_balance = round((totally_user - one_person_owes), 2)
+                user_info[username].append(user_balance) 
+        user_owes_dict = dict()       
+        for username, value in user_info.items():
+            user_owes = 0 
+            for i in range(2, len(value)):
+                user_owes += value[i]
+                user_owes_dict[username] = user_owes
+      
+        for user, owes in user_owes_dict.items():
+            if owes < 0:
+                message = f'С тебя {abs(owes)} тугриков🤸🏻‍♂️'
+            elif owes > 0:
+                message = f'👍Ты в плюсе на {owes} тугриков' 
+            else:
+                message = 'Вот это да, ты в нулину!🥳'
+            user_id = user_info[user][0]
+            send_message(context, user_id, message)
+            db.set_user_owes(user_id, owes)
+    else:
+        user_id = context._user_id_and_data[0]                   
+        owes = db.get_user_owes(user_id)
+        if owes < 0:
+            message = f'Напоминаю, c тебя {abs(owes)} тугриков 🤡'
+        elif owes > 0:
+            message = f'👍Ты в плюсе на {owes} евриков!' 
+        else:
+            message = 'Ты ничего не должен!🥳'
+        send_message(context, user_id, message)    
 
 
 
 def how_much(update, context):
-    message = calculation(context, update)
-    send_message(context, chat_id=update.effective_chat.id, text=message)
+    calculation(update, context)
 
 
 def sum_recognition(update, context):
@@ -86,18 +117,24 @@ def sum_recognition(update, context):
     name = update.message.chat.first_name
     user_id = context._user_id_and_data[0]
     chat_id = update.effective_chat.id
-    wants_to_say = db.public_message_status(user_id)
     all_users = [user[0] for user in (db.get_users())]
     all_usernames = [user[0] for user in (db.get_usernames())]
-        
-        
     if user_id in all_users:
+    
+        wants_to_say = db.public_message_status(user_id) 
         try:
             if wants_to_say == True:
                 for user in all_users:
                     send_message(context, user, message)
                 db.reset_public(user_id)    
                 return
+
+            elif message == 'Поделить всё, с момента регистрации первого участника':
+                db.set_pays_since(user_id)
+                return send_message(context, chat_id, 'Окей!', reply_markup=ReplyKeyboardRemove())  # в будущем добавить дату с которой
+
+            elif message == 'Считать мой долг с текущей даты':  # оставляем значение по умолчанию
+                return send_message(context, chat_id, 'Окей, будем считать с текущего момента', reply_markup=ReplyKeyboardRemove())
 
             elif message == '👀Никого не надо':
                 send_message(context=context, chat_id=chat_id, text='Ладно, не шали так больше🤡', reply_markup=ReplyKeyboardRemove())
@@ -109,14 +146,15 @@ def sum_recognition(update, context):
                 for user_id in all_users:
                     send_message(context=context, chat_id=user_id, text=message, reply_markup=ReplyKeyboardRemove())
                 return
-            validated_message = re.sub(',','.', message)
+            validated_message = re.sub(r'(, )|(,)|(. )', '.', message)
             amount = float(validated_message)
-            calculation(context, update, amount)  
+            calculation(update, context, amount)
         except:
             message = f'{name},это не число! По балде надаю 🤪!'
-            send_message(chat_id=chat_id, text=message, context=context)
+            send_message(chat_id=chat_id, text=message, context=context, reply_markup=ReplyKeyboardRemove())
             logger.error(f'Введено что-то не то: {update.message.text}.Либо не удалось отправить сообщение')
     else:
+        
         message = 'Я не умею считать твои деньги. Чтобы добавиться в чат введи /start'
         send_message(context, chat_id, message)
 
@@ -124,6 +162,7 @@ def sum_recognition(update, context):
 def reset_sum(update, context):
     all_users = [user[0] for user in (db.get_users())]
     db.reset_sum()
+    db.set_user_owes()
     name = update.message.chat.first_name
     message = f'Сумма обнулена пользвателем {name}💪'
     for chat_id in all_users:
@@ -143,8 +182,6 @@ def group_message(update, context):
     chat_id = update.effective_chat.id
     db.set_public(chat_id)
     context.bot.send_message(chat_id=chat_id, text='Теперь можно написать сообщение и его увидят все участники')
-
-
 
 def delete_user(update, context):
     all_usernames = [user[0] for user in (db.get_usernames())]
@@ -179,7 +216,7 @@ def main():
     bot = Bot(token=secret_token)
     bot.send_message(291198651, 'Меня запустили снова, ура!')
     message = get_all_payments()
-    bot.send_message(291198651, message, parse_mode='MarkdownV2')
+    # bot.send_message(291198651, message, parse_mode='MarkdownV2')
     
     updater.dispatcher.add_handler(CommandHandler('start', wake_up))
     updater.dispatcher.add_handler(CommandHandler('reset', reset_sum))
--- db.py
+++ db.py
@@ -14,6 +14,10 @@ class BotDB:
         """Проверяем есть ли юзер в БД"""
         result = self.cursor.execute('SELECT username FROM users WHERE "user_id" = ?',(user_id,))
         return bool(len(result.fetchall()))
+    
+    def get_user_data(self, user_id):
+        result = self.cursor.execute('SELECT user_id, username, pays_since FROM users WHERE "user_id" = ?', (user_id,))
+        return result.fetchone()
 
     def get_users(self):
         """Вытаскиваем все user_id пользователей из БД"""
@@ -24,6 +28,19 @@ class BotDB:
         """Вытаскиваем все username пользователей из БД"""
         result = self.cursor.execute('SELECT "username" FROM users')    
         return result.fetchall()
+    def get_start_date(self, user_id):
+        """Выясняем с какой даты считать должок"""
+        result = self.cursor.execute('SELECT "join_date" FROM users WHERE "user_id" = ?', (user_id))    
+        return result.fetchall() 
+
+    def pays_from_start(self, user_id):
+        """1 - считать сумму с начала, 0 - с даты вступления"""
+        result = self.cursor.execute('SELECT "pays_from_start" FROM users WHERE "user_id" = ?', (user_id,))
+        # result = self.cursor.execute('SELECT "pays_from_start" FROM users WHERE "user_id" = 291198651')
+        
+        return True if result.fetchone()[0] == 1 else False
+
+
 
     def add_user(self, user_id, username):
         """Добавляем пользователя в БД"""
@@ -36,7 +53,7 @@ class BotDB:
         return self.conn.commit()        
 
 
-    def total(self, user_id=None):
+    def total(self, user_id=None, from_date=None):
         """Сколько всего заплатил один пользователь или все вместе"""
         if user_id is not None:
             self.cursor.execute('SELECT SUM(amount) FROM payments WHERE "user_id" = ?',(user_id,))
@@ -59,6 +76,10 @@ class BotDB:
         """Устанавливаем статус жалобы для пользователя"""
         self.cursor.execute('UPDATE users SET "is_complained"=1 WHERE "user_id"=?', (user_id,))
         return self.conn.commit()
+    def set_payment_from_status(self,user_id):
+        """Ставим 0 - юзер хочет платить с момента вступления в чат"""
+        self.cursor.execute('UPDATE users SET "pays_from_start"=0 WHERE "user_id"=?', (user_id,))
+        return self.conn.commit()        
 
     def remove_complain(self,user_id):
         """Удаляем статус жалобы для пользователя"""
@@ -87,6 +108,39 @@ class BotDB:
         """Все платежи в базе на текущий момент"""
         result = self.cursor.execute('SELECT users.username, payments.amount, payments.timestamp FROM "users" JOIN "payments" ON users.user_id=payments.user_id')
         return result.fetchall() 
+
+    def get_period_payments(self, since, until, user_id=None):
+        if user_id is None:
+            result = self.cursor.execute('SELECT SUM(amount) FROM payments WHERE timestamp BETWEEN ? AND ?',(since, until))
+            result = result.fetchall()[0][0]
+            return result if result else 0
+        result = self.cursor.execute('SELECT SUM(amount) FROM payments WHERE user_id=? AND timestamp BETWEEN ? AND ?',(user_id, since, until))
+        result = result.fetchall()[0][0]
+        return result if result else 0  
+
+
+    def set_pays_since(self, user_id):
+        """Устанаваливаем с какой даты платит юзер"""  
+        result = self.cursor.execute('SELECT MIN("pays_since") FROM users')
+        pays_since = result.fetchone()[0]
+        self.cursor.execute('UPDATE users SET "pays_since"=? WHERE "user_id"=?', (pays_since, user_id)) # +
+        return self.conn.commit()
+    def get_pay_periods(self):
+        result = self.cursor.execute('SELECT user_id, username, pays_since FROM users')
+        return result.fetchall()
+    def get_user_qty(self, until):
+        result = self.cursor.execute('SELECT COUNT(user_id) FROM users WHERE "pays_since"<?',(until,))
+        return result.fetchone()[0]
+
+    def set_user_owes(self, user_id=None, user_owes=0):
+        if user_id is None:
+            self.cursor.execute('UPDATE "users" SET "user_owes"=0')
+            return self.conn.commit()
+        self.cursor.execute('UPDATE "users" SET "user_owes"=? WHERE "user_id"=?', (user_owes, user_id)) 
+        return self.conn.commit()
+    def get_user_owes(self, user_id):
+        result = self.cursor.execute('SELECT "user_owes" FROM "users" WHERE "user_id"=?', (user_id,))
+        return result.fetchone()[0]    
 
 
 
